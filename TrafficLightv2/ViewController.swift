03510e5b71fc9d94640d392ff25428e8729677ca
Vladimir <vproshchin@gmail.com>
2022-06-19 18:13:41 +0300

    Logic of color change is added. Still no logic in button title change

--- TrafficLightv2/ViewController.swift
+++ TrafficLightv2/ViewController.swift
@@ -14,16 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet var greenLightView: UIView!
     @IBOutlet var changeColorButton: UIButton!
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         changeColorButton.layer.cornerRadius = 10
         redLightView.layer.cornerRadius = 64
+        redLightView.layer.opacity = 0.5
         yellowLightView.layer.cornerRadius = 64
+        yellowLightView.layer.opacity = 0.5
         greenLightView.layer.cornerRadius = 64
+        greenLightView.layer.opacity = 0.5
     }
 
     @IBAction func changeColorButtonTapped() {
+        if redLightView.layer.opacity == 0.5 && yellowLightView.layer.opacity == 0.5 && greenLightView.layer.opacity == 0.5 {
+            redLightView.layer.opacity = 1
+        } else if redLightView.layer.opacity == 1 {
+            yellowLightView.layer.opacity = 1
+            redLightView.layer.opacity = 0.5
+        } else if yellowLightView.layer.opacity == 1 {
+            greenLightView.layer.opacity = 1
+            yellowLightView.layer.opacity = 0.5
+        } else if greenLightView.layer.opacity == 1 {
+            redLightView.layer.opacity = 1
+            greenLightView.layer.opacity = 0.5
+        }
     }
     
-}
 
+}
